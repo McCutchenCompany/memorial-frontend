6f3a8b5488edbf330409f69ba78b28b70aef0609
Mitch <mitch@linuxacademy.com>
2018-07-12 16:08:14 -0500

    Added routes to app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { RouterModule } from '../../node_modules/@angular/router';
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CreateMemorialModule } from './create-memorial/create-memorial.module';
+import { FindMemorialModule } from './find-memorial/find-memorial.module';
+import { ViewMemorialModule } from './view-memorial/view-memorial.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    AppRoutingModule,
+    BrowserModule,
+    CreateMemorialModule,
+    FindMemorialModule,
+    RouterModule,
+    ViewMemorialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
