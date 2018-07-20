1a859cd866f3e0733c8a688410a67db5a584b19f
Mitch <mitch@linuxacademy.com>
2018-07-20 09:53:57 -0500

    update routing module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 
 import { RouterModule } from '../../node_modules/@angular/router';
+import { routerReducer, StoreRouterConnectingModule } from '../../node_modules/@ngrx/router-store';
+import { StoreModule } from '../../node_modules/@ngrx/store';
+import { environment } from './../environments/environment';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CreateMemorialModule } from './create-memorial/create-memorial.module';
-import { FindMemorialModule } from './find-memorial/find-memorial.module';
-import { ViewMemorialModule } from './view-memorial/view-memorial.module';
+import { appReducer } from './store/app/app.reducer';
 
 @NgModule({
   declarations: [
@@ -15,10 +18,18 @@ import { ViewMemorialModule } from './view-memorial/view-memorial.module';
   imports: [
     AppRoutingModule,
     BrowserModule,
-    CreateMemorialModule,
-    FindMemorialModule,
     RouterModule,
-    ViewMemorialModule
+    StoreModule.forRoot({
+      app: appReducer,
+      router: routerReducer
+    }),
+    StoreRouterConnectingModule.forRoot({
+      stateKey: 'router'
+    }),
+    !environment.production
+      ? StoreDevtoolsModule.instrument({ maxAge: 25 })
+      : [],
+    EffectsModule.forRoot([])
   ],
   providers: [],
   bootstrap: [AppComponent]
