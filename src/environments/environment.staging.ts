7365cdeac188982821fb8c181c0ea51fcfd1e0ac
Mitch <mitch@linuxacademy.com>
2018-12-17 16:45:33 -0600

    updates auth0 audience

--- src/environments/environment.staging.ts
+++ src/environments/environment.staging.ts
@@ -6,7 +6,7 @@ export const environment = {
     clientID: 'ToDTNgJYq3WcLe2KdA7LrnTImrcwzP5C',
     domain: 'mitch-mccutchen.auth0.com',
     responseType: 'token id_token',
-    audience: 'https://mymemorial.co',
+    audience: 'https://memorial-auth/',
     redirectUri: 'https://staging.mymemorial.co/callback',
     scope: 'openid email profile'
   }
