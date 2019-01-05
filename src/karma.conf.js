2673dab503391d0a227c736d8c889a125966ff14
Mitch <mitch@linuxacademy.com>
2019-01-05 14:26:36 -0600

    Add puppeteer

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -1,6 +1,10 @@
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/1.0/config/configuration-file.html
 
+const process = require('process');
+process.env.CHROME_BIN = require('puppeteer').executablePath();
+const browsers = [process.env.BROWSER || 'Chrome'];
+
 module.exports = function (config) {
   config.set({
     basePath: '',
@@ -25,7 +29,16 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
-    singleRun: false
+    browsers,
+    customLaunchers: {
+      HeadlessChrome: {
+        base: 'ChromeHeadless',
+        flags: [
+          '--no-sandbox',
+          '--disable-gpu'
+        ],
+      },
+    },
+    singleRun: false,
   });
 };
