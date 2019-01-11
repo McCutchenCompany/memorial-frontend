72cc95e3b0e641c45e664284e581463fb713a6bd
Mitch <mitch@linuxacademy.com>
2019-01-11 17:00:04 -0600

    Update specs

--- src/app/shared/components/payment-confirmation/payment-confirmation.component.spec.ts
+++ src/app/shared/components/payment-confirmation/payment-confirmation.component.spec.ts
@@ -1,17 +1,43 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material';
+import { BrowserDynamicTestingModule } from '@angular/platform-browser-dynamic/testing';
+import { Store } from '@ngrx/store';
+import { MockMatDialogRef } from '@shared/testing/mockDialogRef';
+import { configureTestSuite } from 'ng-bullet';
 
+import { TestStore } from './../../testing/test-store';
 import { PaymentConfirmationComponent } from './payment-confirmation.component';
 
 describe('PaymentConfirmationComponent', () => {
   let component: PaymentConfirmationComponent;
   let fixture: ComponentFixture<PaymentConfirmationComponent>;
 
-  beforeEach(async(() => {
+  configureTestSuite(() => {
     TestBed.configureTestingModule({
-      declarations: [ PaymentConfirmationComponent ]
+      declarations: [ PaymentConfirmationComponent ],
+      providers: [
+        {
+          provide: MatDialogRef,
+          useClass: MockMatDialogRef
+        },
+        {
+          provide: MAT_DIALOG_DATA,
+          useValue: {}
+        },
+        {
+          provide: Store,
+          useClass: TestStore
+        }
+      ]
     })
-    .compileComponents();
-  }));
+    .overrideModule(BrowserDynamicTestingModule, {
+      set: {
+        entryComponents: [
+          PaymentConfirmationComponent
+        ]
+      }
+    });
+  });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(PaymentConfirmationComponent);
